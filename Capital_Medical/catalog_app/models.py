b9128898cd85b9aa6de0a1c91a3603e13f098486
duckit69 <gherab.radouane@gmail.com>
2026-03-30 17:52:40 +0200

    create a category Model

--- Capital_Medical/catalog_app/models.py
+++ Capital_Medical/catalog_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.CharField(max_length=50)
+    description = models.TextField()
+    active_flag = models.BooleanField(default=True)
+    parent_category = models.ForeignKey(
+        'self', 
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+        )
+    
+    def __str__(self):
+        return f"The category name is {self.name}, slug: {self.slug}, status: {self.active_flag}"
